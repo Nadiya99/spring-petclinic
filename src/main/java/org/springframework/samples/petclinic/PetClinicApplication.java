f4ea84c808ee1c0ea196a4510dcb451ca786afe0
Nadya Doilidova <nadiya888861@gmail.com>
2018-11-04 23:46:14 +0200

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -13,7 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
     public static void main(String[] args) {
-        //test commit
         SpringApplication.run(PetClinicApplication.class, args);
     }
 
